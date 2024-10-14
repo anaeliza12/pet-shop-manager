7e6b3ecb325b09d9eeb404c47fadada1fe8f8c08
anaeliza12 <anaelizaperobelli@gmail.com>
2024-10-14 20:54:35 -0300

    feat: second commit

--- lib/screens/animals/animal.controller.dart
+++ lib/screens/animals/animal.controller.dart
@@ -3,13 +3,13 @@ import 'package:cp/domain/animal.dart';
 import 'package:get/get.dart';
 
 class AnimalController extends GetxController {
- final Rx<bool> _status = Rx(false);
- final animalDataBase = AnimalDataBase.instance;
+  final Rx<bool> _status = Rx(false);
+  final animalDataBase = AnimalDataBase.instance;
 
- AnimalController();
+  AnimalController();
 
-  saveAnimal(Animal value) async{
-
+  saveAnimal(Animal value) async {
+    await animalDataBase.insert(value);
+    _status.value = true;
   }
-
 }
--- lib/screens/animals/animal_create.screen.dart
+++ lib/screens/animals/animal_create.screen.dart
@@ -1,10 +1,11 @@
 import 'package:cp/domain/animal.dart';
+import 'package:cp/screens/animals/animal.controller.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../../util/snippets.dart';
 
 class AnimalCreateScreen extends StatefulWidget {
-  const AnimalCreateScreen ({super.key})
+  const AnimalCreateScreen({super.key})
 
   @override
   State<AnimalCreateScreen> createState() => _AnimalCreateScreenState();
@@ -15,49 +16,57 @@ class _AnimalCreateScreenState extends State<AnimalCreateScreen> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _racaController = TextEditingController();
   final TextEditingController _idadeController = TextEditingController();
-
-_onPressed(){
-  final animal = Animal(
-    name: _nameController.text,
-    raca: _racaController.text,
-    idade: _idadeController.text
-  );
-}
+  late AnimalController _animalController;
 
   @override
-Widget build(BuildContext context) {
-     return Scaffold(
-       appBar: AppBar(
-         title: const Text("Cadastro de Pets"),
-       ),
-       body: Column(
-         children: [
-           Padding(
-             padding: const EdgeInsets.symmetric(vertical: 20),
-             child: TextFormField(
-               controller: _nameController,
-               decoration: inputDecoration(label: "Informe o nome"),
-             ),
-           ),
-           Padding(
-             padding: const EdgeInsets.symmetric(vertical: 20),
-             child: TextFormField(
-               controller: _racaController,
-               decoration: inputDecoration(label: "Informe a raça"),
-             ),
-           ),
-           Padding(
-             padding: const EdgeInsets.symmetric(vertical: 20),
-             child: TextFormField(
-               controller: _idadeController,
-               decoration: inputDecoration(label: "Informe a idade"),
-             ),
-           ),
-            ElevatedButton(onPressed: _onPressed, child: const Text("Entrar")),
+  void initState() {
+    _animalController = AnimalController();
+    super.initState();
+  }
 
-         ],
-       ),
+  _onPressed() {
+    final animal = Animal(
+        name: _nameController.text,
+        raca: _racaController.text,
+        idade: _idadeController.text
+    );
+    _animalController.saveAnimal(animal);
+  }
 
-     );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Cadastro de Pets"),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: TextFormField(
+              controller: _nameController,
+              decoration: inputDecoration(label: "Informe o nome"),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: TextFormField(
+              controller: _racaController,
+              decoration: inputDecoration(label: "Informe a raça"),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: TextFormField(
+              controller: _idadeController,
+              decoration: inputDecoration(label: "Informe a idade"),
+            ),
+          ),
+          ElevatedButton(onPressed: _onPressed, child: const Text("Entrar")),
+
+        ],
+      ),
+
+    );
   }
 }
